236feee3d6fe1ccd5ca5bc46fb3ddd6a2ce7051f
aoticbarker <92726972+aoticwise@users.noreply.github.com>
2022-05-05 11:04:23 -0400

    adding a lot

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+controller.combos.attachCombo("UDLRRLDUABAB", function () {
+    if (CHEAT_CODE_1 == 0) {
+        CHEAT_CODE_1 = 1
+        info.changeScoreBy(100)
+    } else {
+        music.buzzer.play()
+        scene.setBackgroundColor(2)
+        scene.setBackgroundColor(2)
+        scene.setBackgroundColor(2)
+    }
+})
+let CHEAT_CODE_1 = 0
+CHEAT_CODE_1 = 0
